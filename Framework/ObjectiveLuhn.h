3066adf11e5c9e0708c4dd21fe04308221fa20e5
Guilherme Rambo <insidegui@gmail.com>
2017-02-13 15:44:53 -0200

    Carthage support

---
+++ Framework/ObjectiveLuhn.h
@@ -0,0 +1,19 @@
+//
+//  ObjectiveLuhn.h
+//  ObjectiveLuhn
+//
+//  Created by Guilherme Rambo on 13/02/17.
+//  Copyright © 2017 Peixe Urbano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ObjectiveLuhn.
+FOUNDATION_EXPORT double ObjectiveLuhnVersionNumber;
+
+//! Project version string for ObjectiveLuhn.
+FOUNDATION_EXPORT const unsigned char ObjectiveLuhnVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ObjectiveLuhn/PublicHeader.h>
+
+#import <ObjectiveLuhn/Luhn.h>
